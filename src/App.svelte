e66d5b540714d79725ef61bd30383739b9b197a6
Michael LeCompte <michaelslec98@gmail.com>
2021-02-19 02:45:35 -0800

    feat: added initial components

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,9 @@
+<script>
+  import Timer from "./components/Timer.svelte";
+  import HowTo from "./components/HowTo.svelte";
+</script>
+
 <h1>Test</h1>
+
+<Timer />
+<HowTo />
---
+++ src/components/Timer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import ProgressBar from "./ProgressBar.svelte";
+</script>
+
+<p>Timer</p>
+
+<ProgressBar />
