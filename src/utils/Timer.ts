987bb896b9c775193cae67de65a81ba980185e68
Michael LeCompte <michaelslec98@gmail.com>
2021-02-19 04:47:20 -0800

    feat: move timer logic into separate class

---
+++ src/utils/Timer.ts
@@ -0,0 +1,33 @@
+export class Timer {
+  private timerID: NodeJS.Timeout;
+
+  time: number;
+  secondsRemaining: number;
+  isCurrentlyRunning = false;
+
+  constructor(time: number) {
+    this.time = time;
+    this.secondsRemaining = time;
+  }
+
+  countDown = () => {
+    if (this.secondsRemaining == 0) this.stop();
+    else this.secondsRemaining -= 1;
+  };
+
+  start() {
+    if (!this.isCurrentlyRunning) {
+      this.timerID = setInterval(this.countDown, 1000);
+      this.isCurrentlyRunning = true;
+    }
+  }
+
+  stop() {
+    clearTimeout(this.timerID);
+    this.isCurrentlyRunning = false;
+  }
+
+  getPercentage() {
+    return (this.secondsRemaining / this.time) * 100;
+  }
+}
