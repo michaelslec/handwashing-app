9aa1dce67e4c6864554606b3f9078c62c9089214
Michael LeCompte <michaelslec98@gmail.com>
2021-02-19 02:55:10 -0800

    feat: added sound and image attributions

--- src/App.svelte
+++ src/App.svelte
@@ -13,3 +13,10 @@
 
 <Timer />
 <HowTo />
+
+<h3>
+  <a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a>
+  <a href="https://freesound.org/people/metrostock99/sounds/345086/">
+    Sound Source
+  </a>
+</h3>
