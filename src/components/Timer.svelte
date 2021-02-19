df634a7670a1bfdcd6654a1c99a872c1e0062227
Michael LeCompte <michaelslec98@gmail.com>
2021-02-19 07:03:17 -0800

    feat: styling

--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -5,20 +5,8 @@
   import { handwashingTime } from "../constants";
 
   const timer = new Timer(handwashingTime);
-  $: percentage = ($countdownSecondsRemaining / timer.time) * 100;
+  $: percentage = (countdownSecondsRemaining / timer.time) * 100;
 </script>
-
-<style>
-  h2 {
-    margin: 0;
-  }
-
-  .start {
-    background-color: rgb(154, 73, 73);
-    width: 100%;
-    margin: 10px 0;
-  }
-</style>
 
 <div bp="grid">
   <h2 bp="offset-5@md 4@md 12@sm">
@@ -31,7 +19,26 @@
 
 <div bp="grid">
   <button
+    disabled={timer.isCurrentlyRunning}
     on:click={() => timer.start()}
     bp="offset-5@md 4@md 12@sm"
-    class="start">Start</button>
+    class="start">Start</button
+  >
 </div>
+
+<style>
+  h2 {
+    margin: 0;
+  }
+
+  .start {
+    background-color: rgb(154, 73, 73);
+    width: 100%;
+    margin: 10px 0;
+  }
+
+  .start[disabled] {
+    background-color: rgb(194, 194, 194);
+    cursor: not-allowed;
+  }
+</style>
