e09d75f04e511d36a5a978fb37fd45238ecfadf6
Michael LeCompte <michaelslec98@gmail.com>
2021-02-19 03:15:54 -0800

    fix: weird alignment w/ styling

--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -17,6 +17,9 @@
 <div bp="grid">
   <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
 </div>
+
 <ProgressBar />
 
-<button class="start">Start</button>
+<div bp="grid">
+  <button bp="offset-5@md 4@md 12@sm" class="start">Start</button>
+</div>
