96c08c3c4d3d2c7e00d9271402948c4203772a67
Michael LeCompte <michaelslec98@gmail.com>
2021-02-19 02:53:12 -0800

    feat: add handwashing image to how to

--- src/components/HowTo.svelte
+++ src/components/HowTo.svelte
@@ -1,1 +1,12 @@
-<p>how to</p>
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
+
+<div bp="grid">
+  <img
+    bp="offset-4@md 5@md 12@sm"
+    src="handwashing.gif"
+    alt="How to wash your hands" />
+</div>
